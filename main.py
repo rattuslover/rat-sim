e178deb32a3180308803aeedcef57b36c3e9b1d2
skibidirizz69420 <qinhaochu19@gmail.com>
2025-03-13 18:12:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ def DegreeToRadian(degree):
 def GetAngle(pos):
 	return math.atan2(pos[0], pos[1])
 class Player:
-	def __init__(self, health, speed):
+	def __init__(self, health, speed, friction):
 		self.health=health
 		self.speed=speed
 		self.position=[0, 0]
-    self.momentum=[0, 0]
-  def movement(self, direction):
-      if direction=="up":
-        self.momentum[1]+=speed
-class Ground:
+    		self.momentum=[0, 0]
+		self.friction=0.8
+  	def movement(self, direction):
+		if direction=="up":
+			self.momentum[1]-=speed
+		if direction=="down":
+			self.momentum[1]+=speed
+		if direction=="left":
+			self.momentum[0]-=speed
+		if direction=="right":
+			self.momentum[0]+=speed
+		self.momentum[0]=self.momentum[0]*self.friction
+		self.momentum[1]=self.momentum[1]*self.friction
+		self.position[0]+=self.momentum[0]
+		self.position[1]+=self.momentum[1]
+	
